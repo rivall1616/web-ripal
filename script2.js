82c63aeccb8a0a97600aefcc3758c761a010b89e
rivall1616 <ripalripal1616@gmail.com>
2025-08-11 21:54:50 +0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+function TampilkanPromo() {
+  alert('Promo Spesial! Diskon 20% untuk pembelian pertama.');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Website Nusantara Culinary siap dimuat');
+});
